466da2212767b9956cdd9b4d353ab55010cec36f
Chris Goodbro <chrisshamonah@gmail.com>
2024-11-20 17:07:58 +0100

    Added a few variables. Something is incorrect

--- app/src/main/java/com/example/firstexercise/MainActivity.kt
+++ app/src/main/java/com/example/firstexercise/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : ComponentActivity() {
 
@@ -19,6 +21,14 @@ class MainActivity : ComponentActivity() {
             intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
             startActivity(intent)
         }
+
+        val credentialsManager = CredentialsManager()
+
+        val emailInputLayout = findViewById<TextInputLayout>(R.id.EmailAddress) //Email Layout
+        val passwordInputLayout = findViewById<TextInputLayout>(R.id.password) //Password Layout
+
+        val emailInputText = findViewById<TextInputEditText>(R.id.EmailAddress) //Email Text
+        val passwordInputText = findViewById<TextInputEditText>(R.id.password) //Password Text
 
     }
 }
